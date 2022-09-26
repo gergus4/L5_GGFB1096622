f68fc84f82e175770fc37cd964e21512e7959ae2
gerfu <gerfu@HPGERARDO>
2022-09-26 13:37:12 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*EJERCICIO 1*/
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numeroEntero = 0;
+
+        // Mensaje en pantalla//
+        Console.WriteLine("EJERCICIO 01");
+        Console.WriteLine("Número entero ");
+
+
+        //siguiente es recicibir y almacenar//
+
+        numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+        if (numeroEntero >= 1)
+        {
+            Console.WriteLine("Su número es positivo");
+        }
+        else if (numeroEntero <= -1)
+        {
+            Console.WriteLine("Su número es negativo");
+        }
+        else if (numeroEntero == 0)
+        {
+           Console.WriteLine("Su número es igual a 0");
+
+        }
+            }
+        }
